5768834d2dac809d79dccb92b2913c6e38d9400d
Tor (torbuntu) Sorensen <trey-man@live.ru>
2018-03-16 23:09:13 -0500

    Reboot project
    Redoing the project from the ground up. Reimplemented a text handler (still broken so far, will get working eventually). Can write text to the screen. Backspace works. So far so good.

---
+++ desktop/src/com/torbuntu/leikr/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.torbuntu.leikr.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.torbuntu.leikr.Leikr;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Leikr-16");
+		config.setWindowedMode(260, 160);
+		new Lwjgl3Application(new Leikr(), config);
+	}
+}
