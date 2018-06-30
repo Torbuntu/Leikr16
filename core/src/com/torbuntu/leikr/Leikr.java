2510b1d5d142150d9facb2a3515bad44a70d3f74
torbuntu <sorensentor@protonmail.com>
2018-06-29 19:21:27 -0500

    --Removing unused import statements. --Random cleanup

--- core/src/com/torbuntu/leikr/Leikr.java
+++ core/src/com/torbuntu/leikr/Leikr.java
@@ -2,20 +2,7 @@ package com.torbuntu.leikr;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import groovy.lang.GroovyShell;
-import javafx.scene.input.KeyCode;
-
-import java.util.ArrayList;
 
 public class Leikr extends ApplicationAdapter  {
 
