84e341fedd11e1ace05c083098b0aa513a582cb7
torbuntu <sorensentor@protonmail.com>
2018-09-08 09:37:19 -0500

    -- Sound Engine work -- starting to rework the sound engine.

--- core/src/com/leikr/core/SoundEngine/SoundEngine.java
+++ core/src/com/leikr/core/SoundEngine/SoundEngine.java
@@ -14,6 +14,10 @@ import com.jsyn.unitgen.SquareOscillator;
 import com.jsyn.unitgen.TriangleOscillator;
 import com.jsyn.unitgen.WhiteNoise;
 import com.jsyn.unitgen.UnitOscillator;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 /**
  *
@@ -81,6 +85,50 @@ public class SoundEngine {
         lineOut.stop();
         // Stop everything.
         synth.stop();
+        //playTone();
+    }
+
+    public static void playTone() {
+
+        try {
+            createTone(1000, 100, 100);
+        } catch (LineUnavailableException lue) {
+            System.out.println(lue);
+        }
+    }
+
+    /**
+     * parameters are frequency in Hertz and volume
+     *
+     */
+    public static void createTone(int Hertz, int volume, int time)
+            throws LineUnavailableException {
+        /**
+         * Exception is thrown when line cannot be opened
+         */
+
+        float rate = 8000f;
+        byte[] buf;
+        AudioFormat audioF;
+
+        buf = new byte[1];
+        audioF = new AudioFormat(rate, 8, 1, true, false);
+        //sampleRate, sampleSizeInBits,channels,signed,bigEndian
+
+        SourceDataLine sourceDL = AudioSystem.getSourceDataLine(audioF);
+        sourceDL = AudioSystem.getSourceDataLine(audioF);
+        sourceDL.open(audioF);
+        sourceDL.start();
+
+        for (int i = 0; i < time*8; i++) {
+            double angle = i / (rate / Hertz) * 2.0 * Math.PI;
+            buf[0] = (byte) (Math.sin(angle) *127.0 * volume);
+            sourceDL.write(buf, 0, 1);
+        }
+
+        sourceDL.drain();
+        sourceDL.stop();
+        sourceDL.close();
     }
 
 }
